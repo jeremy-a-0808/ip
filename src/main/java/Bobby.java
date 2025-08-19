e9c4630438b0b741f5d96b132ef16002f9164886
jeremy <e1406441@u.nus.edu>
2025-08-19 21:10:16 +0800

    Fixed indentation and underscores

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -5,22 +5,23 @@ public class Bobby {
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        System.out.println("        ______________________________");
-        System.out.println("        Hello! I'm Bobby");
-        System.out.println("        What can I do for you?");
-        System.out.println("        ______________________________");
+        System.out.println("    ______________________________");
+        System.out.println("    Hello! I'm Bobby");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ______________________________");
 
         while (true) {
             input = scanner.nextLine();
+            System.out.println("    ______________________________");
 
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("        Bye. Hope to see you again soon!");
-                System.out.println("        ______________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ______________________________");
                 break;
             }
 
-            System.out.println("        " + input);
-            System.out.println("        ______________________________");
+            System.out.println("    " + input);
+            System.out.println("    ______________________________");
         }
     }
 }
