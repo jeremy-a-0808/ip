27327d028130b024cde37180d94351db35b3f797
jeremy <e1406441@u.nus.edu>
2025-08-19 20:45:49 +0800

    Level 0 Completed

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,10 @@
+public class Bobby {
+    public static void main(String[] args) {
+        System.out.println("______________________________");
+        System.out.println("Hello! I'm Bobby");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________");
+        System.out.println("Hope to see you again soon!");
+        System.out.println("______________________________");
+    }
+}
